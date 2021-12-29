protesilaos/mct

commit 54c3ea65c5bfaabba561c8dfff5e438f41c166b9

keymaps: Use remappings where possible
Remappings help users who use different basic keybindings. I am using remappings
in Vertico and made good experiences so far. Also prefer TAB/RET over the low
level key symbols <return>/<tab> etc. This should work better in terminals.
Emacs internally translates <tab> to TAB.

mct.el
@@ -976,20 +976,19 @@ region.")
 
 (defvar mct-completion-list-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "M-v") #'scroll-down-command)
     (define-key map [remap keyboard-quit] #'mct-keyboard-quit-dwim)
     (define-key map [remap goto-line] #'mct-choose-completion-number)
     (define-key map [remap next-line] #'mct-next-completion-or-mini)
     (define-key map (kbd "n") #'mct-next-completion-or-mini)
     (define-key map [remap previous-line] #'mct-previous-completion-or-mini)
-    (define-key map (kbd "M-p") #'mct-previous-completion-group)
-    (define-key map (kbd "M-n") #'mct-next-completion-group)
     (define-key map (kbd "p") #'mct-previous-completion-or-mini)
+    (define-key map [remap backward-paragraph] #'mct-previous-completion-group)
+    (define-key map [remap forward-paragraph] #'mct-next-completion-group)
     (define-key map (kbd "e") #'mct-focus-minibuffer)
     (define-key map (kbd "M-e") #'mct-edit-completion)
-    (define-key map (kbd "<tab>") #'mct-choose-completion-no-exit)
-    (define-key map (kbd "<return>") #'mct-choose-completion-exit)
-    (define-key map (kbd "<M-return>") #'mct-choose-completion-dwim)
+    (define-key map (kbd "TAB") #'mct-choose-completion-no-exit)
+    (define-key map (kbd "RET") #'mct-choose-completion-exit)
+    (define-key map (kbd "M-RET") #'mct-choose-completion-dwim)
     (define-key map [remap beginning-of-buffer] #'mct-beginning-of-buffer)
     map)
   "Derivative of `completion-list-mode-map'.")
@@ -998,19 +997,19 @@ region.")
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-j") #'exit-minibuffer)
     (define-key map [remap goto-line] #'mct-choose-completion-number)
+    (define-key map [remap next-line] #'mct-switch-to-completions-top)
+    (define-key map [remap next-line-or-history-element] #'mct-switch-to-completions-top)
+    (define-key map [remap previous-line] #'mct-switch-to-completions-bottom)
+    (define-key map [remap previous-line-or-history-element] #'mct-switch-to-completions-bottom)
     (define-key map (kbd "M-e") #'mct-edit-completion)
-    (define-key map (kbd "<C-return>") #'mct-complete-and-exit)
-    (define-key map (kbd "C-n") #'mct-switch-to-completions-top)
-    (define-key map (kbd "<down>") #'mct-switch-to-completions-top)
-    (define-key map (kbd "C-p") #'mct-switch-to-completions-bottom)
-    (define-key map (kbd "<up>") #'mct-switch-to-completions-bottom)
+    (define-key map (kbd "C-RET") #'mct-complete-and-exit)
     (define-key map (kbd "C-l") #'mct-list-completions-toggle)
     map)
   "Derivative of `minibuffer-local-completion-map'.")
 
 (defvar mct-minibuffer-local-filename-completion-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "<backspace>") #'mct-backward-updir)
+    (define-key map (kbd "DEL") #'mct-backward-updir)
     map)
   "Derivative of `minibuffer-local-filename-completion-map'.")
 

commit 9dd3edf19d06da11ea5068f9bf8be8106a145d94

Fix regression in key bindings
It was caused by commit 54c3ea6.

Thanks to José Antonio Ortega Ruiz for reporting the issue:
<https://gitlab.com/protesilaos/mct/-/commit/54c3ea65c5bfaabba561c8dfff5e438f41c166b9#note_797019715>.
And thanks to Daniel Mendler for providing an explanation as to why
"C-RET" is problematic:
<https://gitlab.com/protesilaos/mct/-/commit/54c3ea65c5bfaabba561c8dfff5e438f41c166b9#note_797063627>.

mct.el
@@ -998,7 +998,7 @@ region.")
     (define-key map [remap previous-line] #'mct-switch-to-completions-bottom)
     (define-key map [remap previous-line-or-history-element] #'mct-switch-to-completions-bottom)
     (define-key map (kbd "M-e") #'mct-edit-completion)
-    (define-key map (kbd "C-RET") #'mct-complete-and-exit)
+    (define-key map (kbd "C-<return>") #'mct-complete-and-exit)
     (define-key map (kbd "C-l") #'mct-list-completions-toggle)
     map)
   "Derivative of `minibuffer-local-completion-map'.")