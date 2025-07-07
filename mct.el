306cb704196c643b552aceb5893693641ca747d9
Protesilaos Stavrou <info@protesilaos.com>
2025-07-07 10:41:33 +0300

    Update to mct version 1.1.0

--- mct.el
+++ mct.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/mct
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((emacs "29.1"))
 
 ;; This file is NOT part of GNU Emacs.
